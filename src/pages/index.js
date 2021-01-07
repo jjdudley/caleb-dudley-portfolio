32b2bd939f7da07bb43711a86827b2167c0523dd
Josh Dudley <joshdudley.vt@gmail.com>
2021-01-07 13:36:42 -0500

    additional mobile styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -245,7 +245,7 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mobileView: false,
+      // mobileView: false,
       splashPage: true,
       animateEnter: true
     };
